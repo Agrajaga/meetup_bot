7ef99bad3c1b0e310a3fe1ca20ae4130f1fffda7
Michael <soplomiha@gmail.com>
2022-07-26 23:17:28 +0300

    Add models

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField('имя пользователя', max_length=150)
+    telegram_id = models.CharField('телеграм ИД', max_length=20)
+    telegram_username = models.CharField('телеграм имя', max_length=50)
+    is_speaker = models.BooleanField('докладчик', default=False)
+
+    def __str__(self) -> str:
+        return f'{self.name} @{self.telegram_username}'
+
+
+class Presentation(models.Model):
+    speaker = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField('название', max_length=250)
+    description = models.TextField('описание')
+
+
+class Question(models.Model):
+    presentation = models.ForeignKey(
+        Presentation, on_delete=models.CASCADE, related_name='questions')
+    text = models.TextField('текст вопроса')
+    listener = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    is_active = models.BooleanField('актуальный', default=True)
