e3cd7a7364f9501e44d7c7a45a43748bb4d205ac
Michael <soplomiha@gmail.com>
2022-07-29 00:16:26 +0300

    Add Presentation to admins form

--- bot/admin.py
+++ bot/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Event, EventGroup, Profile, Question
+from .models import Event, EventGroup, Presentation, Profile, Question
 
 admin.site.register(Event)
 admin.site.register(EventGroup)
 admin.site.register(Question)
 admin.site.register(Profile)
+admin.site.register(Presentation)
